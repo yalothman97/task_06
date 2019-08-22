5e295e20d618c5305728414f2177a9f11363f7ac
Yousef Al Othman <Yousef@MacBook-Pro-2.local>
2019-08-22 10:59:33 +0300

    Hessah AlHamadi & Youssef Alothman

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,25 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from .forms import RestaurantForm
+
 
 def welcome(request):
-    return render(request, 'index.html', {'msg':'Hello World!'})
+	return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-    context = {
-        "restaurants":Restaurant.objects.all()
-    }
-    return render(request, 'list.html', context)
+	context = {
+		"restaurants":Restaurant.objects.all()
+	}
+	return render(request, 'list.html', context)
 
 
 def restaurant_detail(request, restaurant_id):
-    context = {
-        "restaurant": Restaurant.objects.get(id=restaurant_id)
-    }
-    return render(request, 'detail.html', context)
+	context = {
+		"restaurant": Restaurant.objects.get(id=restaurant_id)
+	}
+	return render(request, 'detail.html', context)
 
 def restaurant_create(request):
-
-    context = {
-
-    }
-    return render(request, 'create.html', context)
+	form = RestaurantForm()
+	if request.method == "POST":
+		form = RestaurantForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("restaurant-list")
+	context = {
+		'form': form
+	}
+	return render(request, 'create.html', context)
